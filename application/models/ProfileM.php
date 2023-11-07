b5ea8a17d6a52bfa4778ed1145f56b6201229c31
NiharKK <146782092+NiharKK@users.noreply.github.com>
2023-11-07 17:49:56 -0500

    Update ProfileM.php

--- application/models/ProfileM.php
+++ application/models/ProfileM.php
@@ -1,31 +1,114 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-class ProfileM extends CI_Model
+class Profile extends CI_Controller
 {
-	public function selectUserById($data)
+	function __construct()
 	{
-		return $this->db->select("u.*,c.CityName")->from("tbluser u")->join("tblcity c","c.CityId=u.CityId")->where($data)->get()->result();
+		parent::__construct();
+		$this->load->model("ProfileM","pm");
 	}
 
-	public function selectCommunityByUserId($data)
+	public function index($uid)
 	{
-		return $this->db->select("c.*,u.Username,ct.*,cy.CityName")->from("tblcommunity c")->join("tbluser u","u.UserId=c.UserId")->join("tblcategory ct","ct.CategoryId=c.CategoryId")->join("tblcity cy","cy.CityId=c.CityId")->where_in("u.UserId",$data)->get()->result();
+		if (!isset($_SESSION['uname'])) 
+		{
+			redirect("User");
+		}
+		$data=array(
+			"UserId"=>$uid
+		);
+		$temp=array(
+			"User"=>$this->pm->selectUserById($data),
+			"Community"=>$this->pm->selectCommunityByUserId($data),
+			"City"=>$this->pm->selectAllCity(),
+			"Members"=>$this->pm->selectCommunityMemberByUserId($data)
+		);
+		// echo "<pre>";
+		// print_r($temp);
+		// echo "</pre>";
+		$this->load->view("MyProfile",$temp);
 	}
 
-	public function selectAllCity()
+	public function editInfo($uid)
 	{
-		return $this->db->get("tblcity")->result();
+		$where=array(
+			"UserId"=>$uid
+		);
+		$newdata=array(
+			"Email"=>$this->input->post('txtemail'),
+			"Gender"=>$this->input->post('txtgender'),
+			"DOB"=>$this->input->post('txtdob'),
+			"CityId"=>$this->input->post('txtcityid'),
+			"MobileNo"=>$this->input->post('txtmob')
+		);
+		// echo "<pre>";
+		// print_r($newdata);
+		// echo "</pre>";
+		$this->pm->updateUser($newdata,$where);
+		redirect("Profile/index/$uid");
 	}
 
-	public function updateUser($newdata,$where)
+	public function editPassword($uid)
 	{
-		return $this->db->where($where)->update("tbluser",$newdata);
+		$where=array(
+			"UserId"=>$uid
+		);
+		$User=$this->pm->selectUserById($where);
+		$oldpass=$this->input->post('txtoldpassword');
+		if ($oldpass==$User[0]->Password) 
+		{
+			$newdata=array(
+				"Password"=>$this->input->post('txtPwd')
+			);
+			$temp=array(
+				"User"=>$this->pm->selectUserById($where),
+				"Community"=>$this->pm->selectCommunityByUserId($where),
+				"City"=>$this->pm->selectAllCity(),
+				"Members"=>$this->pm->selectCommunityMemberByUserId($where),
+				"UserId"=>$uid,
+				"Err"=>"Password Changed Successfully"
+			);
+			$this->pm->updateUser($newdata,$where);
+			$this->load->view("MyProfile",$temp);
+		}
+		else
+		{
+			$newdata=array(
+				"Msg"=>"Old Password is Incorrect",
+				"User"=>$this->pm->selectUserById($where),
+				"Community"=>$this->pm->selectCommunityByUserId($where),
+				"City"=>$this->pm->selectAllCity(),
+				"Members"=>$this->pm->selectCommunityMemberByUserId($where),
+				"UserId"=>$uid
+		);
+			$this->load->view("MyProfile",$newdata);
+		}
+		
+		// echo "<pre>";
+		// print_r($newdata);
+		// echo "</pre>";
 	}
 
-	public function selectCommunityMemberByUserId($data)
+	public function editImage($uid)
 	{
-		return $this->db->select("cm.*,u.UserId as uid,u.Username,c.*")->from("tblcommunitymember cm")->join("tbluser u","u.UserId=cm.UserId")->join("tblcommunity c","c.CommunityId=cm.CommunityId")->where_in("u.UserId",$data)->get()->result();
+		$where=array(
+			"UserId"=>$uid
+		);
+		$img=$_FILES['fup']['name'];
+		copy($_FILES['fup']['tmp_name'], "C:/xampp/htdocs/DeveloperCommunity/resources/shared/images/".$img) or die("cannot upload image");
+		if ($img=="") 
+		{
+			redirect("Profile/index/$uid");
+		}
+		else
+		{
+			$newdata=array(
+				"ProfileImage"=>$img
+			);
+			$this->pm->updateUser($newdata,$where);
+			$_SESSION['propic']=$img;
+			redirect("Profile/index/$uid");
+		}
 	}
 }
-//return $this->db->select()->from()->join()->get()->result();
 ?>
